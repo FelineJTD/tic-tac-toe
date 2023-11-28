9c2c12d3e3d74bd29e1b91969420e64d375926b9
FelineJTD <13520050@std.stei.itb.ac.id>
2023-11-28 22:34:43 +0100

    chore: initiate nuxt

---
+++ components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <!-- generate unlimited 100px by 100px clickable div -->
+    <div class="flex flex-wrap w-[300vw]">
+      <Box
+        v-for="i in 1800"
+        :key="i"
+        fill=""
+        @click="console.log(i)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Box from './Box.vue';
+
+export default {
+    name: 'Home',
+    components: { Box }
+}
+</script>
+
+<style scoped>
+/* Your component's styles go here */
+</style>
