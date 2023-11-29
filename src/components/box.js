b503b1ca8cbd2186a1c67b75a74b9137a9c1b5f2
FelineJTD <13520050@std.stei.itb.ac.id>
2023-11-29 01:10:38 +0100

    feat: add border

--- src/components/box.js
+++ src/components/box.js
@@ -10,7 +10,7 @@ export default function Box({ }) {
 
   return (
     <button 
-      className="w-[10vw] h-[10vw] lg:w-24 lg:h-24 border"
+      className="w-[10vw] h-[10vw] lg:w-24 lg:h-24 border border-gray-300 dark:border-gray-700"
       onClick={() => fill === '' && (setFill(currentPlayer) || changePlayer())}
       onDoubleClick={() => setFill('')}
     >
